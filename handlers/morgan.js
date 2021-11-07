9cee2fa0a1b73c7947449a6b1a8fdfe33703a8fe
zivserphos <zivfromisrael@gmail.com>
2021-11-07 19:12:53 +0200

    updated morgan

---
+++ handlers/morgan.js
@@ -0,0 +1,9 @@
+const morgan = require("morgan");
+
+function morganHandler(req, res, next) {
+  morgan.token("body", function (req, res) {
+    return JSON.stringify(req.body);
+  });
+}
+
+module.exports = morganHandler;
