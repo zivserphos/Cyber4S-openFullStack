5949751e3750432af4176ec9d6e8e7100f1776f5
DanielPhilosoph <danielph9@gmail.com>
2021-11-07 15:00:31 +0200

    3.2 Done

--- main.js
+++ main.js
@@ -2,12 +2,17 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const presonsRouter = require("./routers/presons");
+const persons = require("./phonebook");
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/persons", presonsRouter);
+
+app.get("/info", (req, res, next) => {
+  res.send(`Phonebook has info for ${persons.length} peaple.\n${new Date()}`);
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
--- routers/presons.js
+++ routers/presons.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { persons } = require("../phonebook");
 const router = express.Router();
 /**
  * *This route routes to:
@@ -9,26 +10,3 @@ router.get("/", (req, res, next) => {
 });
 
 module.exports = router;
-
-const persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
