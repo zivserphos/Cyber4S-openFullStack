30f848addc66427c294c1bd65dd38e070c22740f
DanielPhilosoph <danielph9@gmail.com>
2021-11-07 15:09:19 +0200

    3.3 Done

--- routers/presons.js
+++ routers/presons.js
@@ -1,10 +1,21 @@
 const express = require("express");
-const { persons } = require("../phonebook");
+const persons = require("../phonebook");
 const router = express.Router();
 /**
  * *This route routes to:
  * ? /api/persons
  */
+router.get("/:id", (req, res, next) => {
+  const person = persons.filter((person) => {
+    return parseInt(person.id) === parseInt(req.params.id);
+  });
+  if (person.length === 0) {
+    res.status(404).send("Person not found");
+  } else {
+    res.json(person);
+  }
+});
+
 router.get("/", (req, res, next) => {
   res.send(persons);
 });
