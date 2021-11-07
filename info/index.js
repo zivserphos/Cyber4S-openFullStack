2f5e591217ae7e49b3e1ddf99aa34e0125ce7445
Ziv Serphos <zivfromisrael@gmail.com>
2021-11-07 15:50:14 +0200

    added delete option

--- info/index.js
+++
@@ -1,4 +0,0 @@
-const moment = require("moment")
-const x = moment().format("dddd,MMMM Do YYYY, h:mm:ss a")
-console.log(x)
-document.body.append("sffffffff")
--- routes/infoRouter.js
+++ routes/infoRouter.js
@@ -2,12 +2,15 @@ const express= require("express")
 const infoRouter = express.Router()
 const moment = require("moment")
 const fsAsync = require("fs/promises");
+const { writeFile } = require("fs");
 
 module.exports = infoRouter
 
+const dataBaseFile = async (fileRoot) => JSON.parse(await fsAsync.readFile(`${fileRoot}/data/data.json`))
+
 async function amountPeople() {
     const fileRoot = __dirname.split("routes")[0]
-    const fileData = JSON.parse(await fsAsync.readFile(`${fileRoot}/data/data.json`))
+    const fileData = await dataBaseFile(fileRoot)
     let amount =0;
     for (let obj in fileData) {
         amount+=1;
@@ -20,3 +23,20 @@ infoRouter.get("/" , async (req ,res) => {
     const time = moment().format("dddd,MMMM Do YYYY, h:mm:ss a")
     res.send(`phonebook has info for ${await amountPeople()} people ${time}`)
 })
+
+infoRouter.get("/:id" , async (req,res) => {
+    const fileRoot = __dirname.split("routes")[0]
+    const fileData = await dataBaseFile(fileRoot)
+    const id = Number(req.params.id)
+    const obj = fileData.find((obj) => obj.id === id) 
+    obj ? res.send(obj) : res.status(404).send()
+})
+
+infoRouter.delete("/:id" , async (req,res) => {
+    const fileRoot = __dirname.split("routes")[0]
+    const fileData = await dataBaseFile(fileRoot)
+    const id = Number(req.params.id)
+    const index = fileData.findIndex((obj) => obj.id === id) 
+    index ? fileData.splice(index , 1) : console.log("s")
+    await fsAsync.writeFile("./data/data.json" , JSON.stringify(fileData))
+}) 
