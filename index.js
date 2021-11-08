a5d8147c5254904abbae7eb9cc8058d4f5232225
SimpleLogic420 <simplelogic420@gmail.com>
2021-11-08 10:52:20 +0200

    changed port desination

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ app.use(
 app.use("/api/persons", personRouter);
 app.use("/info/", infoRouter);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
